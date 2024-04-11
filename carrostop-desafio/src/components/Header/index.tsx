72c31e3b9088056375eb8310197b1232cef735c3
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-04-11 20:38:29 -0300

    desafio

---
+++ carrostop-desafio/src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+
+    return (
+        <>
+            <div className="header-container">
+                <h1>Carros Top</h1>
+            </div>
+        </>
+    );
+}
