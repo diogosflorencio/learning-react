69ddafbcbec34e40eccb3c92c9a2e2b55b00d4f0
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-04-11 20:13:03 -0300

    Criando mais componentes

---
+++ projeto-react/src/components/CatalogCard/index.tsx
@@ -0,0 +1,18 @@
+import ComputerImg from '../../assets/computer.png';
+import './styles.css';
+
+export default function CatalogCard() {
+  return (
+    <>
+      <div className="dsc-card">
+        <div className="dsc-catalog-card-top dsc-line-bottom">
+          <img src={ComputerImg} alt="Computer" />
+        </div>
+        <div className="dsc-catalog-card-bottom">
+          <h3>R$ 5000,00</h3>
+          <h4>Computador Gamer XT</h4>
+        </div>
+      </div>
+    </>
+  );
+}
