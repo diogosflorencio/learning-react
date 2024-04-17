0601b5c14e589589ecb6a005d8b328ef4b5b96f7
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-04-16 21:23:00 -0300

    Footer

---
+++ desafio-componentes/src/Footer/index.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function Footer() {
+  return (
+    <>
+      <div className="cd-footer-container">
+        <h3>@dscarrostop</h3>
+        <h3>Rua da Pipoca, 150</h3>
+      </div>
+    </>
+  );
+}
--- desafio-componentes/src/Routes/Home/index.tsx
+++ desafio-componentes/src/Routes/Home/index.tsx
@@ -1,12 +1,17 @@
+import Footer from "../../Footer";
 import Header from "../../Header";
+
+import './styles.css';
 
 export default function Home() {
   return (
     <>
       <Header />
+      <section className="dc-container-1">
+        <h2>Venha nos visitar</h2>
+      </section>
       <section></section>
-      <section></section>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
