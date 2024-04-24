a00b71a4e1ebb38c4d2caa167b7a6db2037c38ba
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-04-23 22:39:52 -0300

    adding routes

--- tutorial-react-router/src/main.tsx
+++ tutorial-react-router/src/main.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Expenses from './routes/Expenses/index.tsx'
-import Invoices from './routes/Invoices/index.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Expenses from "./routes/Expenses/index.tsx";
+import Invoices from "./routes/Invoices/index.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="expenses" element={<Expenses />} />
-      <Route path="invoices" element={<Invoices />} />
-    </Routes>
-  </BrowserRouter>,
-  </React.StrictMode>,
-)
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+      </Routes>
+    </BrowserRouter>
+    ,
+  </React.StrictMode>
+);
