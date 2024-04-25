b0ab58184452a7d154c7a577cbd2887248856367
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-04-24 22:11:33 -0300

    Criando subsrota

--- tutorial-react-router/src/routes/Invoices/index.tsx
+++ tutorial-react-router/src/routes/Invoices/index.tsx
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom";
+import { getInvoices } from "../../data";
+
 export default function Invoices() {
-    return(
-        <main style={{ padding: "1rem 0"}}>
-            <h2>Invoices</h2>
-        </main>
-    )
+  const Invoices = getInvoices();
+  return (
+    <div>
+      <nav>
+        {Invoices.map((invoice) => (
+          <Link
+            style={{ display: "block", margin: "1rem 0" }}
+            to={`/invoices/${invoice.number}`}
+            key={invoice.number}
+          >
+            {invoice.name}
+          </Link>
+        ))}
+      </nav>
+    </div>
+  );
 }
