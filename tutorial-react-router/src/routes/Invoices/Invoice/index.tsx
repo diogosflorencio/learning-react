a031a37ed1e1d769ac8c7f42bbbfad5770135dcc
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-04-25 23:32:50 -0300

    Lendo parametros de url

--- tutorial-react-router/src/routes/Invoices/Invoice/index.tsx
+++ tutorial-react-router/src/routes/Invoices/Invoice/index.tsx
@@ -1,9 +1,20 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import { getInvoice } from "../../../data";
 
-export default function Invoice () {
-    const params = useParams();
-    return(
-
-        <h2>Invoice {params.invoiceId}</h2>
-    );
+export default function Invoice() {
+  const params = useParams();
+  const invoice = getInvoice(parseInt(params.invoiceId, 10));
+  return (
+    <>
+      {invoice && (
+        <main style={{ padding: "1rem" }}>
+          <h2> Total Due: {invoice.due}</h2>
+          <p>
+            {invoice.name}: {invoice.number}
+          </p>
+          <p>Due Date: {invoice.due}</p>
+        </main>
+      )}
+    </>
+  );
 }
