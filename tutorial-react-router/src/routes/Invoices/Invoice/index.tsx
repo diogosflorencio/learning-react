3f698df6c4d5133a7f99276d2bf9df9ee6cd0998
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-05-01 23:33:29 -0300

    Navigating Programmatically

--- tutorial-react-router/src/routes/Invoices/Invoice/index.tsx
+++ tutorial-react-router/src/routes/Invoices/Invoice/index.tsx
@@ -1,7 +1,8 @@
-import { useParams } from "react-router-dom";
-import { getInvoice } from "../../../data";
+import { useNavigate, useParams } from "react-router-dom";
+import { deleteInvoice, getInvoice } from "../../../data";
 
 export default function Invoice() {
+  const navigate = useNavigate();
   const params = useParams();
   const invoice = getInvoice(Number(params.invoiceId));
   return (
@@ -12,7 +13,16 @@ export default function Invoice() {
           <p>
             {invoice.name}: {invoice.number}
           </p>
+          
           <p>Due Date: {invoice.due}</p>
+        <button
+          onClick={() => {
+            deleteInvoice(invoice.number);
+            navigate("/invoices" + location.search);
+          }}
+        >
+          Delete
+        </button>
         </main>
       )}
     </>
