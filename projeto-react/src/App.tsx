62ac7fde72de3ddd8e9bb66415f0236bf1207029
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-05-02 21:30:55 -0300

    Criando rota para catalogo

--- projeto-react/src/App.tsx
+++ projeto-react/src/App.tsx
@@ -1,11 +1,15 @@
 //import ProductDetails from './routes/ProductDetails';
-import Catalog from './routes/Catalog';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Catalog from "./routes/Catalog";
 
 function App() {
-
   return (
-  <Catalog />
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Catalog />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
