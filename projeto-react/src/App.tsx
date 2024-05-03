1c672d4b11890d9ca0211b0af89a473b92109a56
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-05-02 21:33:31 -0300

    sei local

--- projeto-react/src/App.tsx
+++ projeto-react/src/App.tsx
@@ -1,12 +1,15 @@
 //import ProductDetails from './routes/ProductDetails';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Catalog from "./routes/Catalog";
+import ProductDetails from "./routes/ProductDetails";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Catalog />}></Route>
+        <Route path="/" element={<Catalog />}>
+          <Route path="product-details" element={<ProductDetails/>}/>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
