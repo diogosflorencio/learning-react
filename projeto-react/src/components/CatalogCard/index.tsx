d7b0128dfd36a73b79d6da49ad64492e3e06cdee
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-05-07 20:10:06 -0300

    mostrando imagens de cada produto

--- projeto-react/src/components/CatalogCard/index.tsx
+++ projeto-react/src/components/CatalogCard/index.tsx
@@ -1,23 +1,22 @@
 import { Link } from "react-router-dom";
-import ComputerImg from "../../assets/computer.png";
 import { productCatalogDTO } from "../../routes/models/productCatalog";
 import "./styles.css";
 
 type Props = {
-  product: productCatalogDTO;
+  productParam: productCatalogDTO;
 };
 
-export default function CatalogCard({ product }: Props) {
+export default function CatalogCard({ productParam }: Props) {
   return (
     
-      <Link to={`/product-details/${product.id}`}>
+      <Link to={`/product-details/${productParam.id}`}>
         <div className="dsc-card">
           <div className="dsc-catalog-card-top dsc-line-bottom">
-            <img src={ComputerImg} alt="Computer" />
+            <img src={productParam.imgUrl} alt="Computer" />
           </div>
           <div className="dsc-catalog-card-bottom">
-            <h3>{product.price}</h3>
-            <h4>{product.name}</h4>
+            <h3>{productParam.price}</h3>
+            <h4>{productParam.name}</h4>
           </div>
         </div>
       </Link>
--- projeto-react/src/routes/models/productCatalog.ts
+++ projeto-react/src/routes/models/productCatalog.ts
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import { itensInfoDTO } from "./itensInfo";
 
 export type productCatalogDTO = {
+    imgUrl: string | undefined;
     name: ReactNode;
     price: ReactNode;
     id: number,
