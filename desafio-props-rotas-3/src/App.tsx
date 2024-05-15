9ffe8d68abcabd7c3a8d9ed7831acef8a9b8cc72
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-05-14 22:21:07 -0300

    Exercicio 3 concluído

--- desafio-props-rotas-3/src/App.tsx
+++ desafio-props-rotas-3/src/App.tsx
@@ -1,8 +1,7 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import HomeBody from "./routes/Home/HomeBody";
 import Promotion from "./routes/Home/Promotion";
-import Subcribe from "./routes/Subscribe";
 import Subscribe from "./routes/Subscribe";
 
 function App() {
@@ -11,10 +10,11 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} >
-            <Route index element={<HomeBody />} />
+            <Route index element={<Navigate to="/home"/>} />
+            <Route path="home" element={<HomeBody />} />
             <Route path="promotion" element={<Promotion />} />
+            <Route path="subscribe" element={<Subscribe/>}/>
           </Route>
-            <Route path="/subscribe" element={<Subscribe/>}/>
         </Routes>
       </BrowserRouter>
     );
--- desafio-props-rotas-3/src/components/Header/index.tsx
+++ desafio-props-rotas-3/src/components/Header/index.tsx
@@ -1,15 +1,41 @@
-import { Link } from 'react-router-dom';
-import './styles.css';
+import { Link, useLocation, useSearchParams } from "react-router-dom";
+import "./styles.css";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
-
-    return (
-        <header>
-            <div className="container">
-                <Link to="/">
-                    <h1 className="site-name">MeuSite</h1>
-                </Link>
-            </div>
-        </header>
-    );
+  return (
+    <header>
+      <div className="header-content-container container">
+        <Link to="/">
+          <h1 className="site-name">MeuSite</h1>
+        </Link>
+      </div>
+      <nav className="navbar">
+        <NavLink
+          to="/home"
+          className={({ isActive }) =>
+            isActive ? "menu-item menu-active" : "menu-item"
+          }
+        >
+          Início
+        </NavLink>
+        <NavLink
+          to="/promotion"
+          className={({ isActive }) =>
+            isActive ? "menu-item menu-active" : "menu-item"
+          }
+        >
+          Promoção
+        </NavLink>
+        <NavLink
+          to="/subscribe"
+          className={({ isActive }) =>
+            isActive ? "menu-item menu-active" : "menu-item"
+          }
+        >
+          Participar
+        </NavLink>
+      </nav>
+    </header>
+  );
 }
