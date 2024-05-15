bbdf9984851a43309a8ca189d07cdb18718d512b
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-05-14 21:32:13 -0300

    exercicio 3

---
+++ desafio-props-rotas-3/src/routes/Home/Promotion/index.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import Button from "../../../components/Button";
+import Card from "../../../components/Card";
+
+export default function Promotion() {
+
+    return (
+        <main>
+        <section>
+            <div className="container">
+                <div className="mt30 mb30">
+                    <Card title="pagina de promoção" />
+                </div>
+                <div className="dflex">
+                    <Link to="/subscribe"><Button text="Quero participar" /></Link>
+                </div>
+            </div>
+        </section>
+    </main>
+    );
+}
