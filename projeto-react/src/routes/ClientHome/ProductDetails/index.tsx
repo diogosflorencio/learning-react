b2aa5252ad7d2f5f78479e3055fe2580add3deef
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-05-28 22:31:59 -0300

    Catch request error"

--- projeto-react/src/routes/ClientHome/ProductDetails/index.tsx
+++ projeto-react/src/routes/ClientHome/ProductDetails/index.tsx
@@ -3,7 +3,7 @@ import ProductDetailsCard from "../../../components/ProductDetailsCard";
 import Button from "../../../components/Button";
 import ButtonInvertido from "../../../components/ButtonInvertido";
 import * as productService from "../../../services/product-service";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { ProductDTO } from "../../models/product";
@@ -11,13 +11,17 @@ import { ProductDTO } from "../../models/product";
 export default function ProductDetails() {
   const params = useParams();
 
+  const navigate = useNavigate();
+
   const [product, setProduct] = useState<ProductDTO>();
 
   useEffect(() => {
     productService.findById(Number(params.productId)).then((response) => {
-      console.log(response.data);
       setProduct(response.data);
-    });
+    })
+    .catch(() => {
+      navigate("/")
+    })
   }, []);
 
   return (
--- projeto-react/src/services/product-service.ts
+++ projeto-react/src/services/product-service.ts
@@ -2,11 +2,11 @@ import axios from "axios";
 import { BASE_URL } from "../utils/system";
 
 export function findAll() {
-    return axios.get(`${BASE_URL}/products?size=12`)
+    return axios.get(`${BASE_URL}products?size=12`)
 
 }
 
 export function findById(id: number) {
-    return axios.get(`${BASE_URL}/products?size=12/products/${id}`)
+    return axios.get(`${BASE_URL}products?size=12/products/${id}`)
 }
 
