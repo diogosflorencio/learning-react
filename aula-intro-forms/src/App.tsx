ccf6c015a5988040e9e3cba829815b0a1836a623
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-05-29 22:58:09 -0300

    criando formulario e fazendo submit

--- aula-intro-forms/src/App.tsx
+++ aula-intro-forms/src/App.tsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+type FormData = {
+  firstName: string;
+  lastName: string;
 }
 
-export default App
+function App() {
+
+  const [fullName, setFullName] = useState<String>();
+  const [formData, setFormData] = useState<FormData>({
+    firstName: '',
+    lastName: ''
+  })
+
+  function handleInputChange (event: any) {
+    const value = event.target.value;
+    const name = event.target.name;
+    setFormData({...formData, [name]: value})
+  }
+
+  function handleFormSubmit(event: any){
+    event.preventDefault();
+    setFullName(formData.firstName + " " + formData.lastName)
+
+  }
+  return (
+    <>
+    <form onSubmit={handleFormSubmit}>
+      <div>
+        <input
+        name="firstName"
+        value={formData.firstName}
+        type="text"
+        placeholder="Digite seu nome"
+        onChange={handleInputChange}
+        />
+      </div>
+      <div>
+        <input
+        name="lastName"
+        value={formData.lastName}
+        type="text"
+        placeholder="Digite o sobre nome"
+        onChange={handleInputChange}
+        />
+      </div>
+      <button type="submit">Mostrar nome completo</button>
+    </form>
+    <h2>{fullName}</h2>
+    </>
+
+  );
+}
+
+export default App;
