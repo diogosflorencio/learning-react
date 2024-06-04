1c7d38fe37e68286e37a57c27325e1e048a1b727
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-06-04 20:11:55 -0300

    dia anterior

--- desafio-github-api/src/App.tsx
+++ desafio-github-api/src/App.tsx
@@ -1,9 +1,10 @@
 
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import ClientApp from "./routes/ClientApp/Home";
+import ClientApp from "./routes/ClientApp/Inicio";
 import Form from "./routes/ClientApp/Form";
 import Resposta from "./routes/ClientApp/Form/Resposta";
-import Home from "./routes/ClientApp/Home";
+import Home from "./routes/ClientApp/Inicio";
+import Inicio from "./routes/ClientApp/Inicio";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/"  element={<ClientApp/>}>
           <Route index element={<Navigate to="/home" />} />
-          <Route path="home" element={<Home/>}/>
+          <Route path="home" element={<Inicio/>}/>
           <Route path="encontrar" element={<Form />}>
             <Route path="resultado" element={<Resposta/>}/>
           </Route>
