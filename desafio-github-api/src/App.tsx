3fcadcf6d375647490c78a62343795c287694a8d
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-06-04 20:22:02 -0300

    correção das rotas

--- desafio-github-api/src/App.tsx
+++ desafio-github-api/src/App.tsx
@@ -1,18 +1,16 @@
 
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import ClientApp from "./routes/ClientApp/Inicio";
+import ClientApp from "./routes/ClientApp";
 import Form from "./routes/ClientApp/Form";
 import Resposta from "./routes/ClientApp/Form/Resposta";
-import Home from "./routes/ClientApp/Inicio";
 import Inicio from "./routes/ClientApp/Inicio";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"  element={<ClientApp/>}>
-          <Route index element={<Navigate to="/home" />} />
-          <Route path="home" element={<Inicio/>}/>
+        <Route path="/" element={<ClientApp />}>
+          <Route index element={<Inicio/>}/>
           <Route path="encontrar" element={<Form />}>
             <Route path="resultado" element={<Resposta/>}/>
           </Route>
