1b34a57100d6bc2e74e315758a938d9beca687cf
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-06-04 20:22:27 -0300

    Route name fixed

--- desafio-github-api/src/routes/ClientApp/index.tsx
+++ desafio-github-api/src/routes/ClientApp/index.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import Header from "../../components/Header";
 
 
-export default function Home() {
+export default function ClientApp() {
     return (
         <>
         <Header/>
