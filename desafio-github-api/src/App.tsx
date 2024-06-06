a328b78a1e5563b64b50a8ffb01e25019c6e637c
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-06-05 21:41:43 -0300

    nova rota de erro

--- desafio-github-api/src/App.tsx
+++ desafio-github-api/src/App.tsx
@@ -2,8 +2,9 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import ClientApp from "./routes/ClientApp";
 import Form from "./routes/ClientApp/Form";
-import Resposta from "./routes/ClientApp/Form/Resposta";
+import Resposta from "./routes/ClientApp/Form/RespostaErro";
 import Inicio from "./routes/ClientApp/Inicio";
+import RespostaErro from "./routes/ClientApp/Form/RespostaErro";
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
         <Route path="/" element={<ClientApp />}>
           <Route index element={<Inicio/>}/>
           <Route path="encontrar" element={<Form />}>
-            <Route path="resultado" element={<Resposta/>}/>
+            <Route path="erro" element={<RespostaErro/>}/>
+            <Route path="resposta" element={<Resposta/>}/>
           </Route>
         </Route>
       </Routes>
