75dfb572fdbdd4bb374360b12bffdb54a341423d
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-06-11 23:27:37 -0300

    usando localstorage

--- projeto-react/src/routes/ClientHome/Catalog/index.tsx
+++ projeto-react/src/routes/ClientHome/Catalog/index.tsx
@@ -5,11 +5,21 @@ import ButtonNextPage from "../../../components/ButtonNextPage";
 import * as productService from "../../../services/product-service";
 import { useEffect, useState } from "react";
 import { ProductDTO } from "../../models/product";
+import { CategoryDTO } from "../../models/category";
 
 export default function Catalog() {
   const [products, setProducts] = useState<ProductDTO[]>([]);
 
+  const objTest : CategoryDTO = {
+    id: 8,
+    name: "jardinagem"
+  }
+
   useEffect(() => {
+    // localStorage.setItem("minhaCategoria", JSON.stringify(objTest))
+    const obj = JSON.parse(localStorage.getItem("minhaCategoria") || "")
+    console.log(obj.name)
+
     productService.findAll().then((response) => {
       setProducts(response.data.content);
     });
