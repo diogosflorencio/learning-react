afbc5d6f01f60691a40c35b5b1a4cbad5f8d61ae
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-06-13 21:40:58 -0300

    Rota cart criada

--- projeto-react/src/App.tsx
+++ projeto-react/src/App.tsx
@@ -3,15 +3,16 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Catalog from "./routes/ClientHome/Catalog";
 import ProductDetails from "./routes/ClientHome/ProductDetails";
 import ClientHome from "./routes/ClientHome";
+import Cart from "./routes/ClientHome/Cart";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ClientHome />}>
-          <Route path="catalog" element={<Catalog/>}/>
-          <Route path="/" element={<Catalog/>}/>
+          <Route index element={<Catalog/>}/>
           <Route path="product-details/:productId" element={<ProductDetails/>}/>
+          <Route path="cart" element={<Cart />}/>
         </Route>
           <Route path="*" element={<Navigate to={"/"}/>}/>
       </Routes>
