4217719318fd3a1fd132138d3daf36e068b4e54b
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-06-21 22:26:04 -0300

    a

--- projeto-react/src/services/cart-service.ts
+++ projeto-react/src/services/cart-service.ts
@@ -2,5 +2,5 @@ import { OrderDTO } from "../routes/models/order";
 import * as cartRepository from "../localstorage/cart-repository";
 
 export function saveCart(cart: OrderDTO) {
-    cartRepository.save(cart)
+  cartRepository.save(cart);
 }
